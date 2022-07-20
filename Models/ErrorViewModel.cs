1e2600ca4ea79a7b5f5ed9fc8bcad57b7d4a208a
Willian Marciano <willian_marciano@hotmail.com>
2022-07-20 20:56:18 -0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BlogMongoDB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
