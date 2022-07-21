81b4a2adda1b0dfa339d6372c06eb0485f3508bb
Willian Marciano <willian_marciano@hotmail.com>
2022-07-20 21:01:20 -0300

    Adicionado Configuração do MOngoDb, user e role

--- Startup.cs
+++ Startup.cs
@@ -1,32 +1,33 @@
+using BlogMongoDB.Models;
+using BlogMongoDB.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BlogMongoDB
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var mongoDbSettings = Configuration.GetSection(nameof(MongoDbConfig)).Get<MongoDbConfig>();
+
+            services.AddIdentity<ApplicationUser, ApplicationRole>()
+                    .AddMongoDbStores<ApplicationUser, ApplicationRole, Guid>
+                    (
+                        mongoDbSettings.ConnectionString, mongoDbSettings.Name
+                    );
+
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
